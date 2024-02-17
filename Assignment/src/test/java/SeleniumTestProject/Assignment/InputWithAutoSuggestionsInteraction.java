13ec49ea0f99aed1518f7d44bc2f0e6a7f736287
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-02-17 16:31:37 +0800

    added test case: InputWithAutoSuggestionsInteraction

--- Assignment/src/test/java/SeleniumTestProject/Assignment/InputWithAutoSuggestionsInteraction.java
+++ Assignment/src/test/java/SeleniumTestProject/Assignment/InputWithAutoSuggestionsInteraction.java
@@ -1,10 +1,25 @@
 package SeleniumTestProject.Assignment;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class InputWithAutoSuggestionsInteraction extends CommonFunctions {
 
-	public static void main(String[] args) {
-
-
+	public static void main(String[] args) throws InterruptedException {
+		CommonFunctions cf = new CommonFunctions();
+		WebDriver driver = launchBrowser("chrome", cf.driver);
+		
+		WebElement input = driver.findElement(By.id("autocomplete"));
+		input.sendKeys("India");
+		Thread.sleep(500);
+		input.sendKeys(Keys.DOWN);
+		Thread.sleep(500);
+		input.sendKeys(Keys.DOWN);
+		Thread.sleep(500);
+		input.sendKeys(Keys.ENTER);
+		
+		closeBrowser(driver, 3);
 	}
 
 }
