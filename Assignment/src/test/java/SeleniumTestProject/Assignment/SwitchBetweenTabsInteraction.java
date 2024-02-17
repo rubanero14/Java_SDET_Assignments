264a6f831809d51bd6a3d696df5cb99761870469
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-02-18 01:44:10 +0800

    added test case: TableInteraction

--- Assignment/src/test/java/SeleniumTestProject/Assignment/SwitchBetweenTabsInteraction.java
+++ Assignment/src/test/java/SeleniumTestProject/Assignment/SwitchBetweenTabsInteraction.java
@@ -1,9 +1,19 @@
 package SeleniumTestProject.Assignment;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 public class SwitchBetweenTabsInteraction extends CommonFunctions {
 
 	public static void main(String[] args) {
-
+		CommonFunctions cf = new CommonFunctions();
+		WebDriver driver = launchBrowser("chrome", cf.driver);
+		
+		
 	}
 
 }
--- Assignment/src/test/java/SeleniumTestProject/Assignment/TableInteraction.java
+++ Assignment/src/test/java/SeleniumTestProject/Assignment/TableInteraction.java
@@ -1,9 +1,35 @@
 package SeleniumTestProject.Assignment;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class TableInteraction extends CommonFunctions {
 
 	public static void main(String[] args) {
-
+		CommonFunctions cf = new CommonFunctions();
+		WebDriver driver = launchBrowser("chrome", cf.driver);
+		
+		List<WebElement> tableOne = driver.findElements(By.name("courses"));
+		for (WebElement tableOneItem : tableOne) {
+			System.out.println(tableOneItem.getText());
+		}
+		
+		System.out.println();
+		System.out.println("=============================");
+		System.out.println();
+		
+		Actions action = new Actions(driver);
+		List<WebElement> tableTwo = driver.findElements(By.cssSelector(".tableFixHead #product"));
+		WebElement tableTwoWrapper = driver.findElement(By.cssSelector(".tableFixHead"));
+		action.scrollByAmount(0, 500).scrollToElement(tableTwoWrapper).perform();
+		
+		for (WebElement tableTwoItem : tableTwo) {
+			System.out.println(tableTwoItem.getText());
+		}
+		
+		closeBrowser(driver, 5);
 	}
 
 }
