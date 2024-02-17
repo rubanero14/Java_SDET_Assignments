aaa45d605b93932b58f3fdc1eab9858eaeca6195
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-02-17 20:03:08 +0800

    added test case: DropdownInteraction

--- Assignment/src/test/java/SeleniumTestProject/Assignment/DropdownInteraction.java
+++ Assignment/src/test/java/SeleniumTestProject/Assignment/DropdownInteraction.java
@@ -1,10 +1,19 @@
 package SeleniumTestProject.Assignment;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class DropdownInteraction extends CommonFunctions {
 
 	public static void main(String[] args) {
+		CommonFunctions cf = new CommonFunctions();
+		WebDriver driver = launchBrowser("chrome", cf.driver);
 		
-
+		Select dropdown = new Select(driver.findElement(By.id("dropdown-class-example")));
+		dropdown.selectByVisibleText("Option2");;
+		
+		closeBrowser(driver, 3);
 	}
 
 }
